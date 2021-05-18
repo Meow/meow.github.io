bbb039ad11ba430811448f6f0a6429f463f90f50
Luna D <cod7777@yandex.ru>
2021-05-18 23:12:48 +0200

    teeext

--- js/components/index.jsx
+++ js/components/index.jsx
@@ -2,7 +2,25 @@ import React from "react";
 
 class Index extends React.Component {
   render() {
-    return <h1>Hello</h1>;
+    return <div className="description">
+      <h1>Nighty Cloud, aka Luna</h1>
+      <p>Artist, programmer, silly butt, and many other things</p>
+      <h3>Artist</h3>
+      <p>I've been drawing since 2014, not being too consistent with my art, however trying my best to improve and learn new things, never refusing a challenge when it arose. My main motivation for art is to just make others happy, or otherwise bring you fun and enjoyment through my drawings! Since the death of Tumblr it's been very hard for me to get back up to speed, however I'm trying now ^^</p>
+      <h3>Programmer</h3>
+      <p>I've been coding since 2011, starting off with Garry's Mod Lua, and moving on to web development. In 2017 I was invited to work on Derpibooru, which caused my web development and sysadmin skills to skyrocket, turning me from a clueless stupid into a competent developer... hopefully. I'm experienced in Ruby, Lua, C, as well as most of web dev stack (CSS, HTML, and such).</p>
+      <h3>Silly butt</h3>
+      <p>While introverted, I still love talking to people. I just have a lot of trouble making contact and getting to know new people. If I get into the right groove, I'll probably turn into a chatterbox and reveal all of my secrets to you on accident.</p>
+      <h1>My characters</h1>
+      <p>All of the characters linked above are my own and have been designed by me. While I cannot do anything in particular to prevent you from doing anything with them, please:
+        <ul>
+          <li>Do not roleplay publicly as my characters</li>
+          <li>Do not publish drawings of my characters in scenarios I would dislike (you may get the general idea <a href="https://derpibooru.org/filters/112134">from this filter</a>)</li>
+          <li>Do not claim my characters as your own</li>
+        </ul>
+      </p>
+      <p>Most of my characters are very meaningful to me, and sit in a special place in my heart. I'd love for them to bring you joy as well, but please be considerate to me, okay?</p>
+    </div>;
   }
 }
 
--- js/components/layouts/_navigation.jsx
+++ js/components/layouts/_navigation.jsx
@@ -3,10 +3,11 @@ import React from "react";
 class Navigation extends React.Component {
   render() {
     return <div className="navigation">
-      <a className="navigation__link" href="#">Home</a>
-      <a className="navigation__link" href="#">Nighty Cloud</a>
-      <a className="navigation__link" href="#">Luna the Enfield</a>
-      <a className="navigation__link" href="#">Midnight Story</a>
+      <a className="navigation__link" href="#"><i className="fa fa-home"></i> Home</a>
+      <a className="navigation__link" href="#"><i className="fa fa-horse"></i> Nighty Cloud</a>
+      <a className="navigation__link" href="#"><i className="fa fa-horse"></i> Midnight Story</a>
+      <a className="navigation__link" href="#"><i className="fa fa-dog"></i> Luna the Enfield</a>
+      <a className="navigation__link" href="#"><i className="fa fa-dog"></i> Fletch the Enfield</a>
     </div>;
   }
 }
