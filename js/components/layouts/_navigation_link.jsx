54db83dde23e2dcec0d9185836b0b6df3eee28cc
Luna D <cod7777@yandex.ru>
2021-07-24 13:11:35 +0200

    responsive design

--- js/components/layouts/_navigation_link.jsx
+++ js/components/layouts/_navigation_link.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function NavigationLink(props) {
-  return <a href={props.href} className="navigation__link"><i className={`fa ${props.icon}`}></i> {props.name}</a>;
+  return <a href={props.href} className="navigation__link"><i className={`fa ${props.icon}`}></i><span class="hidden--phone"> {props.name}</span></a>;
 }
 
 export default NavigationLink;
--- js/components/layouts/page_container.jsx
+++ js/components/layouts/page_container.jsx
@@ -1,19 +1,18 @@
 import React from "react";
 import Navigation from "./_navigation";
 
-class PageContainer extends React.Component {
-  render() {
-    return <div className="container">
-      <Navigation></Navigation>
-      <br />
+function PageContainer(props) {
+  return <div className="container">
+    <Navigation></Navigation>
+    <div className="content">
       <div className="block block--information block--solid">
         <div className="block__content">
           <i className="fa fa-wrench"></i> This site is still WIP. The information is incomplete.
         </div>
       </div>
-      {this.props.children}
-    </div>;
-  }
+      {props.children}
+    </div>
+  </div>;
 }
 
 export default PageContainer;
