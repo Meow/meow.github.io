99cbbe488c51df5fb503c9abd8adba5d7b388013
Luna D. <luna@nighty.cloud>
2025-12-03 13:55:09 +0100

    copy cname

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,20 @@
 import { defineConfig } from "vite";
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 import react from "@vitejs/plugin-react";
 import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    react()
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'CNAME',
+          dest: '.',
+        },
+      ],
+    }),
   ],
   build: {
     outDir: "docs"
